5769036cc935e659f2c87ca817581b08bf94d2c2
BreakingBran <lancepereira99@gmail.com>
2017-04-24 00:50:50 -0400

    too tired to continue, but too much fun

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setTitle("Lance's project");
+		scene.getStylesheets().add("application/application.css");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
